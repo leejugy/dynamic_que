1c22fba8c78d508b32bfaa454ebc3a7163f4a84c
leejunggyun <133378372+leejugy@users.noreply.github.com>
2024-12-04 10:49:17 +0900

    Update dynamic_que.c
    sem add

--- dynamic_que.c
+++ dynamic_que.c
@@ -8,18 +8,22 @@ void init_que(dynamic_que_t *q)
         perror("sem init");
     }
 
+    sem_wait(&q->sem);
     q->front = (list_t *)malloc(sizeof(list_t));
     q->rear = q->front;
+    sem_post(&q->sem);
 }
 
 void push_que(dynamic_que_t *q, uint8_t *buffer, size_t buffer_size)
 {
+    sem_wait(&q->sem);
     q->rear->buf = (uint8_t *)malloc(buffer_size * sizeof(uint8_t));
     q->rear->next = (list_t *)malloc(sizeof(list_t));
 
     memcpy(q->rear->buf, buffer, buffer_size);
     q->rear->buf_size = buffer_size;
     q->rear = q->rear->next;
+    sem_post(&q->sem);
 }
 
 int pop_que(dynamic_que_t *q, uint8_t *buffer, size_t buffer_size)
@@ -37,7 +41,8 @@ int pop_que(dynamic_que_t *q, uint8_t *buffer, size_t buffer_size)
         printf("pop que size error\n");
         return -1;
     }
-
+    
+    sem_wait(&q->sem);
     memcpy(buffer, q->front->buf, q->front->buf_size);
 
     q->front = q->front->next;
@@ -45,6 +50,7 @@ int pop_que(dynamic_que_t *q, uint8_t *buffer, size_t buffer_size)
     
     free(temp->buf);
     free(temp);
+    sem_post(&q->sem);
 
     return len;
 }
